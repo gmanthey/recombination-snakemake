89cee2f39dee3b1c18a245c00d92047c2eb13d98
Georg Langebrake <g.langebrake@posteo.de>
2026-08-31 09:05:00 +0200

    bugfix: import snakemake.shell correctly, filter vcf for individuals correctly

--- workflow/scripts/optimize.py
+++ workflow/scripts/optimize.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from typing import TYPE_CHECKING
+from snakemake import shell
 
 if TYPE_CHECKING:
-    from snakemake.iocontainers import snakemake, shell
+    from snakemake.iocontainers import snakemake
 
 def ranks_per_col(data: pd.DataFrame, sense: list):
     ranks = pd.DataFrame(index=data.index, columns=data.columns, dtype=int)
